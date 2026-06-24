c31999d3ca532638fe51470183f8b92cf366dc97
maryem <maryyemhanyy@gmail.com>
2026-06-24 17:01:07 +0300

    Add project files.

---
+++ E-Commerce.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using E_Commerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name).HasColumnType("nvarchar(100)");
+
+            builder.Property(p => p.Description).HasColumnType("nvarchar(500)");
+
+            builder.Property(p => p.PictureUrl).HasColumnType("nvarchar(200)");
+
+            builder.Property(p => p.Price).HasColumnType("decimal(18, 2)");
+
+            builder.HasOne(p => p.ProductBrand).WithMany().HasForeignKey(p => p.BrandId);
+
+            builder.HasOne(p => p.ProductType).WithMany().HasForeignKey(p => p.TypeId);
+
+
+
+        }
+    }
+}
---
+++ E-Commerce.Infrastructure/InfrastructureServiceRegisteration.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure
+{
+    public static class InfrastructureServiceRegisteration
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
